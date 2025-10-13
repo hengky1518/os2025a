4799ab7056a128b457baa46458aeaf11e00880b7
henky <bush1518@naver.com>
2025-10-13 11:19:51 +0900

    loops/main.go v0.2 bufio & os package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	var now time.Time = time.Now()
+	month := now.Month()
+	fmt.Println(month)
 	r := bufio.NewReader(os.Stdin)
-	i, _ := r.ReadString('\n')
+	i, err := r.ReadString('\n') //ignore error
+
+	// fmt.Println(err)
+	log.Fatal(err) //report the error and exit program
 	fmt.Println(i)
 }
