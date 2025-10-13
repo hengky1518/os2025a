dae8f582a35142190b5c2f1e73e7441bc9c7221a
henky <bush1518@naver.com>
2025-10-13 10:48:33 +0900

    loops/main.go v0.1 time & string package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"time"
 )
 
 func main() {
-	//casting
-	var length float64 = 1.2
-	var width int = 2
-	fmt.Println("면적은", int(length)*width)
-	fmt.Println("길이 > 너비?", int(length) > width)
-	fmt.Println("형변환", reflect.TypeOf(float64(length)))
-	fmt.Println("원본", reflect.TypeOf(length))
+	var now time.Time = time.Now()
+	var day int = now.Day()
+	fmt.Println(day)
+	// broken := "G# r#cks!"
+	// replacer := strings.NewReplacer("#", "o")
+	// fixed := replacer.Replace(broken)
+	// fmt.Println(fixed)
 }
