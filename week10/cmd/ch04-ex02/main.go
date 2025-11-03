339b17436aecee9ddf64a768b2f91c56437f43c5
han99417 <bush1518>
2025-11-03 11:13:00 +0900

    week10 package v0.3

---
+++ week10/cmd/ch04-ex02/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"week10/pkg/keyboard"
+)
+
+func main() {
+
+	fmt.Print("점수 입력: ")
+	score, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if score >= 80 {
+		fmt.Printf("%.1f점은 합격!", score)
+	} else {
+		fmt.Printf("%.1f점은 불합격...", score)
+	}
+}
