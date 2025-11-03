014a6f2bc5546f318ff48af419eacc7225150ca3
han99417 <bush1518>
2025-11-03 11:31:34 +0900

    week10 package v0.4

---
+++ week10/cmd/ch04-ex04/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"week10/pkg/greeting"
+	"week10/pkg/korean"
+	deutsch "week10/pkg/korean"
+)
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+	korean.Anyunghaseyo()
+	deutsch.GutenTag()
+}
