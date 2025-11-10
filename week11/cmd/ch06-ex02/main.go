18b01756eaa9c835d52b6170fafaae0fde6fa7af
han99417 <bush1518>
2025-11-10 12:00:51 +0900

    week11 array v0.8

---
+++ week11/cmd/ch06-ex02/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/headfirstgo/datafile"
+)
+
+func main() {
+	weights, err := datafile.GetFloats("meatWeight.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	hap := 0.0
+	//for _, number := range numbers {
+	for i := 0; i < len(weights); i++ {
+		hap = hap + weights[i]
+	}
+	weeks := float64(len(weights))
+	fmt.Println("주차별 고기 소비량 평균:", hap/weeks)
+}
+
+func GetFloats(fileName string) ([3]float64, error) {
+	var numbers [3]float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return numbers, err
+	}
+	i := 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		numbers[i], err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		i++
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+	return numbers, nil
+}
