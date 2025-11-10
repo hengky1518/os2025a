908991a62344c2a6f4bc377248e6b302de661f4f
han99417 <bush1518>
2025-11-10 11:37:52 +0900

    week11 slice v0.4

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -6,11 +6,12 @@ func main() {
 	subjects := []string{"GO", "Javascript", "Python", "Linux"}
 
 	subjectsSlice := subjects[1:3]
-
+	//subjects[0] = "Java"
+	subjectsSlice[0] = "Java"
 	for _, subjects := range subjects {
 		fmt.Println(subjects)
 	}
-
+	fmt.Println("==========")
 	for i := 0; i < len(subjectsSlice); i++ {
 		fmt.Println(subjectsSlice[i])
 	}
