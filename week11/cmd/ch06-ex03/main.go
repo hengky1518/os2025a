e75db8d30aab76800f1ff98dfa511f3714c0029f
han99417 <bush1518>
2025-12-01 10:30:07 +0900

    week14 interface v0.1

---
+++ week11/cmd/ch06-ex03/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/headfirstgo/datafile"
+)
+
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return nil, err
+		}
+		numbers = append(numbers, number)
+	}
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+	return numbers, nil
+}
+
+func main() {
+	weights, err := datafile.GetFloats("meatWeight.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	hap := 0.0
+	//for _, number := range numbers {
+	for i := 0; i < len(weights); i++ {
+		hap = hap + weights[i]
+	}
+	weeks := float64(len(weights))
+	fmt.Println("주차별 고기 소비량 평균:", hap/weeks)
+}
